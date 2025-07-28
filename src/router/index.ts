7d6bb91ddbc771b490132b2cebaa3e406677075c
brayden-aldrich <aldricbr@oregonstate.edu>
2025-07-28 13:24:46 -0400

    Remove lazy load from SubmittedView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import SubmittedView from "@/views/SubmittedView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,11 +11,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/Submitted",
     name: "Submitted",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/SubmittedView.vue"),
+    component: SubmittedView,
   },
 ];
 
